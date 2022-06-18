98e507f59d9b17899bbf05c58b679604d4150a93
Matt Seabold <matt.seabold@gmail.com>
2022-06-17 22:41:19 -0400

    Add initial unit test framework

---
+++ tests/cpu/cpu_tester.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unity/unity.h>
+#include "cpu.h"
+#include "sys.h"
+
+typedef struct mem_expect_s
+{
+    bool rwb;
+    uint16_t addr;
+    uint8_t val;
+} mem_expect_t;
+
+const mem_expect_t test_cpu_reset_mem_expect[] =
+{
+    {true, 0xfffc, 0x34},
+    {true, 0xfffd, 0x12},
+    {true, 0x1234, 0xea}
+};
+
+const mem_expect_t test_cpu_irq_exepect[] =
+{
+    {true, 0xfffc, 0x34},
+    {true, 0xfffd, 0x12},
+    {true, 0x1234, 0x58},
+    {false, 0x01fd, 0x12},
+    {false, 0x01fc, 0x35},
+    {false, 0x01fb, 0x20},
+    {true, 0xfffe, 0x78},
+    {true, 0xffff, 0x56},
+};
+
+static const mem_expect_t *test_mem_calls;
+static unsigned int num_test_mem_calls;
+static unsigned int test_mem_idx;
+
+void test_mem_write(uint16_t addr, uint8_t val)
+{
+    printf("Write 0x%04x 0x%02x\n", addr, val);
+    TEST_ASSERT(test_mem_idx < num_test_mem_calls);
+    TEST_ASSERT_FALSE(test_mem_calls[test_mem_idx].rwb);
+    TEST_ASSERT(test_mem_calls[test_mem_idx].addr == addr);
+    TEST_ASSERT(test_mem_calls[test_mem_idx].val == val);
+    ++test_mem_idx;
+}
+
+uint8_t test_mem_read(uint16_t addr)
+{
+    uint8_t val;
+
+    printf("Read 0x%04x\n", addr);
+    TEST_ASSERT(test_mem_idx < num_test_mem_calls);
+    TEST_ASSERT(test_mem_calls[test_mem_idx].rwb);
+    TEST_ASSERT(test_mem_calls[test_mem_idx].addr == addr);
+
+    val = test_mem_calls[test_mem_idx].val;
+
+    ++test_mem_idx;
+    return val;
+}
+
+
+
+void test_cpu_reset_mem(void)
+{
+    sys_cxt_t cxt = sys_init(test_mem_read, test_mem_write);
+
+    TEST_ASSERT_NOT_NULL(cxt);
+
+    test_mem_calls = test_cpu_reset_mem_expect;
+    num_test_mem_calls = 3;
+    test_mem_idx = 0;
+
+    cpu_init(cxt, true);
+    cpu_step();
+
+    TEST_ASSERT_EQUAL_UINT(test_mem_idx, 3);
+}
+
+void test_cpu_irq(void)
+{
+    sys_cxt_t cxt = sys_init(test_mem_read, test_mem_write);
+
+    TEST_ASSERT_NOT_NULL(cxt);
+
+    test_mem_calls = test_cpu_irq_exepect;
+    num_test_mem_calls = 7;
+    test_mem_idx = 0;
+
+    cpu_init(cxt, true);
+    cpu_step();
+    sys_vote_interrupt(cxt, false, true);
+    TEST_ASSERT(sys_check_interrupt(cxt, false));
+    printf("step\n");
+    cpu_step();
+}
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_cpu_reset_mem);
+    RUN_TEST(test_cpu_irq);
+    return UNITY_END();
+}
---
+++ tests/io/at28c256_tester.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unity/unity.h>
+
+#include "sys.h"
+#include "at28c256.h"
+
+static sys_cxt_t sys;
+static at28c256_t iut;
+
+uint8_t read_hlr(uint16_t addr)
+{
+    return 0xff;
+}
+
+void write_hlr(uint16_t addr, uint8_t val)
+{
+}
+
+void setUp(void)
+{
+    sys = sys_init(read_hlr, write_hlr);
+    iut = at28c256_init(sys, 0);
+}
+
+void tearDown(void)
+{
+    at28c256_destroy(iut);
+    sys_destroy(sys);
+}
+
+static const uint8_t TEST_DATA[] =
+{
+    0x00, 0x01, 0x02, 0x03
+};
+
+void test_image_load(void)
+{
+    TEST_ASSERT(at28c256_load_image(iut, sizeof(TEST_DATA), (uint8_t *)TEST_DATA, 0));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[0], at28c256_read(iut, 0));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[1], at28c256_read(iut, 1));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[2], at28c256_read(iut, 2));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[3], at28c256_read(iut, 3));
+}
+
+void test_image_load_offset(void)
+{
+    TEST_ASSERT(at28c256_load_image(iut, sizeof(TEST_DATA), (uint8_t *)TEST_DATA, 1));
+    TEST_ASSERT_EQUAL_UINT8(0xFF, at28c256_read(iut, 0));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[0], at28c256_read(iut, 1));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[1], at28c256_read(iut, 2));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[2], at28c256_read(iut, 3));
+    TEST_ASSERT_EQUAL_UINT8(TEST_DATA[3], at28c256_read(iut, 4));
+}
+
+void test_image_load_offset_too_large(void)
+{
+    TEST_ASSERT_FALSE(at28c256_load_image(iut, sizeof(TEST_DATA), (uint8_t *)TEST_DATA, 0x7ffd));
+}
+
+void test_single_byte_write(void)
+{
+    uint8_t write_byte, read_byte, last_read;
+
+    write_byte = 0xAA;
+
+    at28c256_write(iut, 0x0000, write_byte);
+
+    read_byte = at28c256_read(iut, 0x0000);
+
+    TEST_ASSERT_BIT_LOW(7, read_byte);
+
+    last_read = read_byte;
+    read_byte = at28c256_read(iut, 0x0000);
+    TEST_ASSERT((last_read ^ read_byte) == 0x40);
+
+    last_read = read_byte;
+
+    at28c256_tick(iut, 160);
+
+    at28c256_tick(iut, 15000);
+
+    TEST_ASSERT(at28c256_read(iut, 0x0000) == write_byte);
+}
+
+void test_partial_page_write(void)
+{
+    uint8_t large_test_data[64];
+    uint8_t i;
+
+    for(i=0;i<64;++i)
+    {
+        large_test_data[i] = 0xee;
+    }
+
+    TEST_ASSERT(at28c256_load_image(iut, sizeof(large_test_data), large_test_data, 0x1000));
+
+    at28c256_write(iut, 0x1004, 0x04);
+    large_test_data[0x04] = 0x04;
+    at28c256_tick(iut, 8);
+
+    at28c256_write(iut, 0x1010, 0x10);
+    large_test_data[0x10] = 0x10;
+    at28c256_tick(iut, 8);
+
+    at28c256_write(iut, 0x1001, 0x01);
+    large_test_data[0x01] = 0x01;
+    at28c256_tick(iut, 8);
+
+    at28c256_write(iut, 0x103f, 0x3f);
+    large_test_data[0x3f] = 0x3f;
+
+    at28c256_tick(iut, 15000);
+
+    for(i=0;i<64;++i)
+    {
+        TEST_ASSERT_EQUAL_UINT8(large_test_data[i], at28c256_read(iut, 0x1000+i));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_image_load);
+    RUN_TEST(test_image_load_offset);
+    RUN_TEST(test_image_load_offset_too_large);
+    RUN_TEST(test_single_byte_write);
+    RUN_TEST(test_partial_page_write);
+    return UNITY_END();
+}
