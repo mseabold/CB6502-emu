4a3308846a6db45a4585ff25e1d143ea689920e4
Matt Seabold <matt.seabold@gmail.com>
2023-12-27 00:28:50 -0500

    Rework selection of ncurses header name

--- frontend/ncurses/inc/curs_common.h
+++ frontend/ncurses/inc/curs_common.h
@@ -1,16 +1,6 @@
 #ifndef __CURS_COMMON_H__
 #define __CURS_COMMON_H__
 
-#if defined(CURSES_HAVE_NCURSES_H)
-#include <ncurses.h>
-#elif defined(CURSES_HAVE_NCURSES_NCURSES_H)
-#include <ncurses/ncurses.h>
-#elif defined(CURSES_HAVE_CURSES_H)
-#include <curses.h>
-#elif defined(CURSES_HAVE_NCURSES_CURSES_H)
-#include <ncurses/curses.h>
-#else
-#error "Unable to determine location of ncurses header"
-#endif
+#include CURSES_HEADER
 
 #endif /* end of include guard: __CURS_COMMON_H__ */
